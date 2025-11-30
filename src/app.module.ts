b5836d03f9b5f270afb96f5f9cee73c3da783df9
pyj <wodus331@gmail.com>
2025-12-01 00:40:09 +0900

    add: rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
+const isDev = process.env.NODE_ENV === 'development';
+console.log('isDve', isDev);
 @Module({
-  imports: [CoreModule],
+  imports: [
+    CoreModule,
+    ThrottlerModule.forRoot([
+      { name: 'default', limit: 60, ttl: 60000, skipIf: () => isDev },
+      { name: 'short', ttl: 1000, limit: 3, skipIf: () => isDev },
+      { name: 'medium', ttl: 10000, limit: 20, skipIf: () => isDev },
+      { name: 'long', ttl: 60000, limit: 100, skipIf: () => isDev },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
