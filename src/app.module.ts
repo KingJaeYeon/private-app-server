2569284b9f6e186bdd5fd17c71fb1e00980d7226
pyj <wodus331@gmail.com>
2025-12-01 16:15:02 +0900

    add: users 기본파일

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { UsersModule } from './modules/users/users.module';
 
 const isDev = process.env.NODE_ENV === 'development';
 console.log('isDve', isDev);
@@ -16,6 +17,7 @@ console.log('isDve', isDev);
       { name: 'medium', ttl: 10000, limit: 20, skipIf: () => isDev },
       { name: 'long', ttl: 60000, limit: 100, skipIf: () => isDev },
     ]),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
