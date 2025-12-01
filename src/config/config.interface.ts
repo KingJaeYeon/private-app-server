e1d81a6924b2656a348548ca6975b796bd3adda5
pyj <wodus331@gmail.com>
2025-12-02 00:52:44 +0900

    update: config jwt, domain 추가

--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -1,13 +1,20 @@
 export interface ConfigKey {
   db: DbConfig;
   app: AppConfig;
+  jwt: JWTConfig;
 }
 
 export interface DbConfig {
   postgresql: string;
 }
 
+export interface JWTConfig {
+  secret: string;
+  expiresIn: number;
+}
+
 export interface AppConfig {
   port: number;
   front: string;
+  domain: string;
 }
