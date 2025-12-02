3bd7c0343c1bf204045e97ea8b0cd9901ed0713a
pyj <wodus331@gmail.com>
2025-12-02 14:16:47 +0900

    add: app.controller system api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { All, Controller, Get, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from '@/common/decorators';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,21 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Public()
+  @All('.well-known/*')
+  @HttpCode(204)
+  wellKnown() {}
+
+  @Public()
+  @Get('favicon.ico')
+  @HttpCode(204)
+  favicon() {}
+
+  @Public()
+  @Get('health')
+  health() {
+    return { status: 'ok' };
   }
 }
