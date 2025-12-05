44b9b0507cc919ea1d08faf77b531e175f20cf15
pyj <wodus331@gmail.com>
2025-12-06 01:52:06 +0900

    update: CurrentUser example

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -2,6 +2,12 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from '@generated/prisma/client';
 import { IJwtPayload } from '@/modules/auth/strategies/jwt.strategy';
 
+/**
+ * @example
+ * CurrentUser('userId')
+ * CurrentUser('email')
+ * CurrentUser()
+ */
 export const CurrentUser = createParamDecorator((data: keyof IJwtPayload | undefined, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
   const user = request.user as User;
