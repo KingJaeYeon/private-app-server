870690c6d76b2bfe30f0fad0a507918866fdb480
pyj <wodus331@gmail.com>
2025-12-07 00:37:38 +0900

    fix: dto 주석 줄내림 삭제

--- src/modules/channels/dto/index.ts
+++ src/modules/channels/dto/index.ts
@@ -3,3 +3,13 @@ export * from './update-subscription.dto';
 export * from './channel-response.dto';
 export * from './channel-history.dto';
 
+export enum ChannelOrderByEnum {
+  viewCount = 'viewCount',
+  subscriberCount = 'subscriberCount',
+  createdAt = 'createdAt'
+}
+
+export enum ChannelOrderEnum {
+  asc = 'asc',
+  desc = 'desc'
+}
