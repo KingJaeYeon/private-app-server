5e7f260ce664be3b1d546c619365637bc70c5033
pyj <wodus331@gmail.com>
2025-12-10 18:06:15 +0900

    update: jwt error 'jwt expired' 추가

--- src/modules/auth/guards/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { Reflector } from '@nestjs/core';
 import { IS_PUBLIC_KEY } from '@/common/decorators';
 import { CustomException } from '@/common/exceptions';
+import { TokenExpiredError } from '@nestjs/jwt';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -20,8 +21,17 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     return super.canActivate(context);
   }
 
-  handleRequest(err, user) {
+  handleRequest(err: any, user: any, info: any) {
     // You can throw an exception based on either "info" or "err" arguments
+    if (info instanceof TokenExpiredError) {
+      throw new CustomException('UNAUTHORIZED', { message: 'jwt expired' });
+    }
+    // // 2) JWT 형식/서명 문제 등 (잘못된 토큰)
+    // if (info instanceof JsonWebTokenError) {
+    //   // console.log('Invalid JWT:', info.message);
+    //   throw new CustomException('UNAUTHORIZED');
+    // }
+
     if (err || !user) {
       throw new CustomException('UNAUTHORIZED');
     }
